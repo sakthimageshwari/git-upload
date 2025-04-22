f5d7d47e29206a572e71be7b65dcfe99ea93f56b
sakthimageshwari <67864820+sakthimageshwari@users.noreply.github.com>
2025-04-22 16:44:07 +0530

    and_or

---
+++ and.py
@@ -0,0 +1,6 @@
+un=input("enter name:")
+pw=input("enter password:")
+if (un=="admin1" and pw=="pass@123") or (un=="admin2" and pw=="pass@456"):
+    print("valid user")
+else:
+    print("invalid user")
